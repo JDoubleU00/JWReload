a404ce6ad1a0df227a82d95742f4305f3247c294
John Wilkerson <jdoubleu00@gmail.com>
2018-12-25 02:12:54 -0500

    Add files via upload

---
+++ JWReload.lua
@@ -0,0 +1,4 @@
+-- To add a new slashcommand, just add  SLASH_RELOAD4 = "/<InputNameHere>" at the end, if you add more remember to change the 4 to 5 and so on.
+
+SlashCmdList["JWRELOAD"] = function() ReloadUI() end
+SLASH_JWRELOAD1 = "/rl"
